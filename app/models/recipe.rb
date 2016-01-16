a5b22fbbb2e317e1a8c6f994e5d0b567cc80e933
newu <newaxisu@yahoo.com>
2016-01-16 06:03:40 +0000

    uploader working

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,17 @@ class Recipe < ActiveRecord::Base
   belongs_to :chef
   validates :chef_id, presence: true
   validates :name, presence: true
+  validates :summary, presence: true
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
   #validates :summary, length: { minimum: 20}
   #validates :description, length: { in: 20..500}
+  
+  private
+    def picture_size
+      if picture.size >5.megabytes
+        errors.add(:picture, "should be less than 5 MB")
+      end
+    end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115030034) do
+ActiveRecord::Schema.define(version: 20160116044553) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160115030034) do
     t.integer  "chef_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "picture"
   end
 
   create_table "styles", force: :cascade do |t|
